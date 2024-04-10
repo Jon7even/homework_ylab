6c2ef619b3441c6d02b1ef1aa98a6b5754f5c0af
JonSeven <mazahaker5555@yandex.ru>
2024-04-11 01:25:55 +0300

    initial commit: base structure

---
+++ src/main/java/com/github/jon7even/TrainingDiaryApp.java
@@ -0,0 +1,7 @@
+package com.github.jon7even;
+
+public class TrainingDiaryApp {
+    public static void main(String[] args) {
+        System.out.println("Ну что ж! Начнем же...");
+    }
+}
---
+++ src/test/java/TrainingDiaryAppTest.java
@@ -0,0 +1,14 @@
+import com.github.jon7even.TrainingDiaryApp;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TrainingDiaryAppTest {
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testMan() {
+        Assertions.assertDoesNotThrow(TrainingDiaryApp::new);
+    }
+}
