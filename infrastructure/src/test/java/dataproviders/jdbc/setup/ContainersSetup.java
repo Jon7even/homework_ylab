756dc3f04fdd995bc01111867eba39e80630c65f
JonSeven <mazahaker5555@yandex.ru>
2024-04-25 23:54:03 +0300

    feat: realised tests for TypeWorkoutJdbcRepository

--- infrastructure/src/test/java/dataproviders/jdbc/setup/ContainersSetup.java
+++ infrastructure/src/test/java/dataproviders/jdbc/setup/ContainersSetup.java
@@ -7,7 +7,9 @@ import com.github.jon7even.dataproviders.core.LiquibaseManager;
 import com.github.jon7even.dataproviders.core.impl.LiquibaseManagerImpl;
 import com.github.jon7even.dataproviders.jdbc.DiaryJdbcRepository;
 import com.github.jon7even.dataproviders.jdbc.HistoryUserJdbcRepository;
+import com.github.jon7even.dataproviders.jdbc.TypeWorkoutJdbcRepository;
 import com.github.jon7even.dataproviders.jdbc.UserJdbcRepository;
+import dataproviders.jdbc.TypeWorkoutJdbcRepositoryTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,6 +24,7 @@ public class ContainersSetup extends PreparationForTests {
     protected static UserJdbcRepository userJdbcRepository;
     protected static HistoryUserJdbcRepository historyUserJdbcRepository;
     protected static DiaryJdbcRepository diaryJdbcRepository;
+    protected static TypeWorkoutJdbcRepository typeWorkoutJdbcRepository;
     protected static MainConfig mainConfig;
     protected static ConfigLoader configLoader;
     protected static LiquibaseManager liquibaseManager;
@@ -45,6 +48,7 @@ public class ContainersSetup extends PreparationForTests {
         liquibaseManager = new LiquibaseManagerImpl(mainConfig);
         historyUserJdbcRepository = new HistoryUserJdbcRepository(mainConfig);
         diaryJdbcRepository = new DiaryJdbcRepository(mainConfig);
+        typeWorkoutJdbcRepository = new TypeWorkoutJdbcRepository(mainConfig);
     }
 
     @AfterEach
